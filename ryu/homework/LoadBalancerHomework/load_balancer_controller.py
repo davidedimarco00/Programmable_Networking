4bdcec95263d735ea6bd816180df65e1cb820af5
davidedimarco00 <davide.dimarco5@studio.unibo.it>
2025-10-05 11:01:52 +0200

    add

--- ryu/homework/LoadBalancerHomework/load_balancer_controller.py
+++ ryu/homework/LoadBalancerHomework/load_balancer_controller.py
@@ -6,206 +6,216 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet, arp, ipv4, tcp, ether_types
 
 
-class RoundRobinLB(app_manager.RyuApp):
+class RoundRobinLoadBalancer(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     VIP_IP   = '192.168.1.100'
     VIP_MAC  = '00:00:00:00:01:00'   # MAC “virtuale” del VIP
-    TCP_PORT = 8080
+    VIP_TCP_PORT = 8080
 
-    # I MAC/port dei backend verranno appresi a runtime (ARP o traffico IP)
+    #I backend vengono appresi a runtime
     BACKENDS = [
-        {'ip': '192.168.1.101', 'mac': None, 'port': None},
-        {'ip': '192.168.1.102', 'mac': None, 'port': None},
+        {'ip': '192.168.1.101', 'mac': None, 'switch_port': None},
+        {'ip': '192.168.1.102', 'mac': None, 'switch_port': None},
     ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # L2 learning: {dpid: {mac: port}}
-        self.mac_to_port = {}
-        # Round-robin pointer
-        self.rr = 0
-        # Stickiness per flusso client (cli_ip, cli_tcp_src) -> backend dict
-        self.flow_map = {}
+        self.mac_learning_table = {}   # {dpid: {mac: port}}
+        self.round_robin_index = 0
 
-    # -------------------- Helpers --------------------
+   #function
 
-    def add_flow(self, dp, priority, match, actions, idle=180, hard=0):
-        ofp, p = dp.ofproto, dp.ofproto_parser
-        inst = [p.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
-        mod = p.OFPFlowMod(datapath=dp, priority=priority, match=match,
-                           instructions=inst, idle_timeout=idle, hard_timeout=hard)
-        dp.send_msg(mod)
+    def add_flow(self, datapath, priority, match, actions, idle_timeout=180, hard_timeout=0):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        flow_mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=instructions,
+            idle_timeout=idle_timeout,
+            hard_timeout=hard_timeout
+        )
+        datapath.send_msg(flow_mod)
 
-    def get_client_out_port(self, dpid, cli_mac, default_port):
-        """Ritorna la porta per il MAC del client, se nota, altrimenti default_port."""
-        table = self.mac_to_port.get(dpid, {})
-        return table.get(cli_mac, default_port)
+    def get_client_output_port(self, switch_id, client_mac, default_port):
+        """Ritorna la porta verso il client, se nota, altrimenti default_port."""
+        mac_table = self.mac_learning_table.get(switch_id, {})
+        return mac_table.get(client_mac, default_port)
 
-    def send_arp_reply(self, dp, in_port, dst_mac, dst_ip):
-        p, ofp = dp.ofproto_parser, dp.ofproto
-        e = ethernet.ethernet(dst=dst_mac, src=self.VIP_MAC,
-                              ethertype=ether_types.ETH_TYPE_ARP)
-        a = arp.arp(opcode=arp.ARP_REPLY,
-                    src_mac=self.VIP_MAC, src_ip=self.VIP_IP,
-                    dst_mac=dst_mac,   dst_ip=dst_ip)
+    def send_arp_reply(self, datapath, client_port, client_mac, client_ip):
+        """Risponde a una ARP request per il VIP."""
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+        eth_reply = ethernet.ethernet(
+            dst=client_mac, src=self.VIP_MAC,
+            ethertype=ether_types.ETH_TYPE_ARP
+        )
+        arp_reply = arp.arp(
+            opcode=arp.ARP_REPLY,
+            src_mac=self.VIP_MAC, src_ip=self.VIP_IP,
+            dst_mac=client_mac, dst_ip=client_ip
+        )
         pkt = packet.Packet()
-        pkt.add_protocol(e)
-        pkt.add_protocol(a)
+        pkt.add_protocol(eth_reply)
+        pkt.add_protocol(arp_reply)
         pkt.serialize()
-        actions = [p.OFPActionOutput(in_port)]
-        out = p.OFPPacketOut(datapath=dp, buffer_id=ofp.OFP_NO_BUFFER,
-                             in_port=ofp.OFPP_CONTROLLER, actions=actions, data=pkt.data)
-        dp.send_msg(out)
-        self.logger.info("ARP reply: %s is-at %s → %s", self.VIP_IP, self.VIP_MAC, dst_mac)
+        actions = [parser.OFPActionOutput(client_port)]
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=ofproto.OFP_NO_BUFFER,
+            in_port=ofproto.OFPP_CONTROLLER,
+            actions=actions,
+            data=pkt.data
+        )
+        datapath.send_msg(out)
+        self.logger.info("ARP reply: %s is-at %s → %s", self.VIP_IP, self.VIP_MAC, client_mac)
 
-    def arp_probe(self, dp, target_ip):
-        """Invia un ARP request broadcast per imparare MAC/porta del backend target_ip."""
-        p, ofp = dp.ofproto_parser, dp.ofproto
+    def send_arp_probe(self, datapath, target_ip):
+        """Invia un ARP request broadcast per scoprire MAC/porta del backend target_ip."""
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
         pkt = packet.Packet()
-        pkt.add_protocol(ethernet.ethernet(dst='ff:ff:ff:ff:ff:ff',
-                                           src=self.VIP_MAC,
-                                           ethertype=ether_types.ETH_TYPE_ARP))
-        pkt.add_protocol(arp.arp(opcode=arp.ARP_REQUEST,
-                                 src_mac=self.VIP_MAC, src_ip=self.VIP_IP,
-                                 dst_mac='00:00:00:00:00:00', dst_ip=target_ip))
+        pkt.add_protocol(ethernet.ethernet(
+            dst='ff:ff:ff:ff:ff:ff',
+            src=self.VIP_MAC,
+            ethertype=ether_types.ETH_TYPE_ARP
+        ))
+        pkt.add_protocol(arp.arp(
+            opcode=arp.ARP_REQUEST,
+            src_mac=self.VIP_MAC, src_ip=self.VIP_IP,
+            dst_mac='00:00:00:00:00:00', dst_ip=target_ip
+        ))
         pkt.serialize()
-        actions = [p.OFPActionOutput(ofp.OFPP_FLOOD)]
-        out = p.OFPPacketOut(datapath=dp, buffer_id=ofp.OFP_NO_BUFFER,
-                             in_port=ofp.OFPP_CONTROLLER, actions=actions, data=pkt.data)
-        dp.send_msg(out)
+        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=ofproto.OFP_NO_BUFFER,
+            in_port=ofproto.OFPP_CONTROLLER,
+            actions=actions,
+            data=pkt.data
+        )
+        datapath.send_msg(out)
         self.logger.info("ARP probe inviato per %s (FLOOD)", target_ip)
 
     # -------------------- Handlers --------------------
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-        dp = ev.msg.datapath
-        ofp, p = dp.ofproto, dp.ofproto_parser
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
-        # Table-miss: invia i pacchetti al controller
-        match = p.OFPMatch()
-        actions = [p.OFPActionOutput(ofp.OFPP_CONTROLLER, ofp.OFPCML_NO_BUFFER)]
-        inst = [p.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
-        mod = p.OFPFlowMod(datapath=dp, priority=0, match=match, instructions=inst)
-        dp.send_msg(mod)
+        # Table-miss: manda i pacchetti al controller
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        instructions = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        flow_mod = parser.OFPFlowMod(datapath=datapath, priority=0, match=match, instructions=instructions)
+        datapath.send_msg(flow_mod)
 
-        # (opzionale) Pre-probe dei backend per popolare mac/port rapidamente
-        for b in self.BACKENDS:
-            self.arp_probe(dp, b['ip'])
+        # Pre-probe dei backend
+        for backend in self.BACKENDS:
+            self.send_arp_probe(datapath, backend['ip'])
 
-        self.logger.info("Switch %s configurato (table-miss + pre-probe).", dp.id)
+        self.logger.info("Switch %s configurato (table-miss + pre-probe).", datapath.id)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in(self, ev):
         msg = ev.msg
-        dp = msg.datapath
-        ofp, p = dp.ofproto, dp.ofproto_parser
-        dpid = dp.id
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        switch_id = datapath.id
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        eth_frame = pkt.get_protocols(ethernet.ethernet)[0]
 
         # L2 learning
-        self.mac_to_port.setdefault(dpid, {})
-        self.mac_to_port[dpid][eth.src] = in_port
+        self.mac_learning_table.setdefault(switch_id, {})
+        self.mac_learning_table[switch_id][eth_frame.src] = in_port
 
         # --- ARP handling ---
-        if eth.ethertype == ether_types.ETH_TYPE_ARP:
-            a = pkt.get_protocol(arp.arp)
-            # Rispondi per il VIP
-            if a.opcode == arp.ARP_REQUEST and a.dst_ip == self.VIP_IP:
-                self.send_arp_reply(dp, in_port, a.src_mac, a.src_ip)
+        if eth_frame.ethertype == ether_types.ETH_TYPE_ARP:
+            arp_pkt = pkt.get_protocol(arp.arp)
+            if arp_pkt.opcode == arp.ARP_REQUEST and arp_pkt.dst_ip == self.VIP_IP:
+                self.send_arp_reply(datapath, in_port, arp_pkt.src_mac, arp_pkt.src_ip)
                 return
 
-            # Apprendi MAC/port dei backend
-            for b in self.BACKENDS:
-                if a.src_ip == b['ip']:
-                    b['mac'], b['port'] = eth.src, in_port
+            # Apprendi backend da ARP
+            for backend in self.BACKENDS:
+                if arp_pkt.src_ip == backend['ip']:
+                    backend['mac'], backend['switch_port'] = eth_frame.src, in_port
 
-            # L2 forward basico per il resto dell’ARP
-            out_port = self.mac_to_port[dpid].get(eth.dst, ofp.OFPP_FLOOD)
-            actions = [p.OFPActionOutput(out_port)]
-            out = p.OFPPacketOut(datapath=dp, buffer_id=ofp.OFP_NO_BUFFER,
-                                 in_port=in_port, actions=actions, data=msg.data)
-            dp.send_msg(out)
+            # Forward normale per ARP
+            out_port = self.mac_learning_table[switch_id].get(eth_frame.dst, ofproto.OFPP_FLOOD)
+            actions = [parser.OFPActionOutput(out_port)]
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                      in_port=in_port, actions=actions, data=msg.data)
+            datapath.send_msg(out)
             return
 
-        # --- Solo IPv4 da qui in poi ---
-        if eth.ethertype != ether_types.ETH_TYPE_IP:
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        if not ip_pkt:
             return
 
-        ip4 = pkt.get_protocol(ipv4.ipv4)
-        if not ip4:
+        tcp_pkt = pkt.get_protocol(tcp.tcp)
+        if not tcp_pkt:
             return
 
-        # Apprendi MAC/port dei backend anche da traffico IP
-        for b in self.BACKENDS:
-            if ip4.src == b['ip']:
-                b['mac'], b['port'] = eth.src, in_port
+        # --- Gestione delle connessioni TCP verso VIP:8080 ---
+        if ip_pkt.dst == self.VIP_IP and tcp_pkt.dst_port == self.VIP_TCP_PORT:
+            client_ip, client_mac = ip_pkt.src, eth_frame.src
+            client_tcp_src_port = tcp_pkt.src_port
 
-        # Gestiamo solo TCP e solo verso VIP:8080
-        tcph = pkt.get_protocol(tcp.tcp)
-        if not tcph:
-            return
+            backend = self.BACKENDS[self.round_robin_index]
+            self.round_robin_index = (self.round_robin_index + 1) % len(self.BACKENDS)
 
-        if ip4.dst == self.VIP_IP and tcph.dst_port == self.TCP_PORT:
-            cli_ip, cli_mac = ip4.src, eth.src
-            cli_tcp_src = tcph.src_port
-            key = (cli_ip, cli_tcp_src)
+            self.logger.info("Nuova connessione %s:%d → VIP:%d → backend %s",
+                             client_ip, client_tcp_src_port, self.VIP_TCP_PORT, backend['ip'])
 
-            backend = self.flow_map.get(key)
-            if backend is None:
-                backend = self.BACKENDS[self.rr]
-                self.rr = (self.rr + 1) % len(self.BACKENDS)
-                self.flow_map[key] = backend
-                self.logger.info("Nuova conn %s:%d → VIP:%d → scelto backend %s",
-                                 cli_ip, cli_tcp_src, self.TCP_PORT, backend['ip'])
+            backend_ip  = backend['ip']
+            backend_mac = backend.get('mac')
+            backend_port = backend.get('switch_port')
 
-            srv_ip  = backend['ip']
-            srv_mac = backend.get('mac')
-            s_out   = backend.get('port')
-
-            # Se non conosciamo ancora MAC/porta del server scelto → ARP probe e aspettiamo
-            if not srv_mac or not s_out:
-                self.arp_probe(dp, srv_ip)
+            # Se non conosciamo ancora MAC/porta del backend scelto
+            if not backend_mac or not backend_port:
+                self.send_arp_probe(datapath, backend_ip)
                 return
 
-            # ---- Flow client -> server (riscrittura dst IP/MAC) ----
-            match_c2s = p.OFPMatch(eth_type=0x0800, ip_proto=6,
-                                   ipv4_src=cli_ip, ipv4_dst=self.VIP_IP,
-                                   tcp_src=cli_tcp_src, tcp_dst=self.TCP_PORT)
-            actions_c2s = [
-                p.OFPActionSetField(eth_src=self.VIP_MAC),
-                p.OFPActionSetField(eth_dst=srv_mac),
-                p.OFPActionSetField(ipv4_dst=srv_ip),
-                p.OFPActionOutput(s_out),
+            # ---- Flusso client -> backend ----
+            match_c2b = parser.OFPMatch(
+                eth_type=0x0800, ip_proto=6,
+                ipv4_src=client_ip, ipv4_dst=self.VIP_IP,
+                tcp_src=client_tcp_src_port, tcp_dst=self.VIP_TCP_PORT
+            )
+            actions_c2b = [
+                parser.OFPActionSetField(eth_src=self.VIP_MAC),
+                parser.OFPActionSetField(eth_dst=backend_mac),
+                parser.OFPActionSetField(ipv4_dst=backend_ip),
+                parser.OFPActionOutput(backend_port),
             ]
-            self.add_flow(dp, 100, match_c2s, actions_c2s, idle=120)
+            self.add_flow(datapath, 100, match_c2b, actions_c2b, idle_timeout=120)
 
-            # ---- Flow server -> client (riscrittura src IP/MAC) ----
-            c_out = self.get_client_out_port(dpid, cli_mac, default_port=in_port)
-            match_s2c = p.OFPMatch(eth_type=0x0800, ip_proto=6,
-                                   ipv4_src=srv_ip, ipv4_dst=cli_ip,
-                                   tcp_src=self.TCP_PORT, tcp_dst=cli_tcp_src)
-            actions_s2c = [
-                p.OFPActionSetField(eth_src=self.VIP_MAC),
-                p.OFPActionSetField(eth_dst=cli_mac),
-                p.OFPActionSetField(ipv4_src=self.VIP_IP),
-                p.OFPActionOutput(c_out),
+            # ---- Flusso backend -> client ----
+            client_out_port = self.get_client_output_port(switch_id, client_mac, default_port=in_port)
+            match_b2c = parser.OFPMatch(
+                eth_type=0x0800, ip_proto=6,
+                ipv4_src=backend_ip, ipv4_dst=client_ip,
+                tcp_src=self.VIP_TCP_PORT, tcp_dst=client_tcp_src_port
+            )
+            actions_b2c = [
+                parser.OFPActionSetField(eth_src=self.VIP_MAC),
+                parser.OFPActionSetField(eth_dst=client_mac),
+                parser.OFPActionSetField(ipv4_src=self.VIP_IP),
+                parser.OFPActionOutput(client_out_port),
             ]
-            self.add_flow(dp, 100, match_s2c, actions_s2c, idle=120)
+            self.add_flow(datapath, 100, match_b2c, actions_b2c, idle_timeout=120)
 
-            # Inoltra subito il pacchetto corrente lato server
-            out = p.OFPPacketOut(datapath=dp, buffer_id=ofp.OFP_NO_BUFFER,
-                                 in_port=in_port, actions=actions_c2s, data=msg.data)
-            dp.send_msg(out)
+            # Inoltra subito il pacchetto corrente
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                      in_port=in_port, actions=actions_c2b, data=msg.data)
+            datapath.send_msg(out)
             return
-
-        # Per altro traffico: opzionale, fai un semplice L2 forward
-        out_port = self.mac_to_port[dpid].get(eth.dst, ofp.OFPP_FLOOD)
-        actions = [p.OFPActionOutput(out_port)]
-        out = p.OFPPacketOut(datapath=dp, buffer_id=ofp.OFP_NO_BUFFER,
-                             in_port=in_port, actions=actions, data=msg.data)
-        dp.send_msg(out)
--- ryu/homework/LoadBalancerHomework/load_balancer_topo.py
+++ ryu/homework/LoadBalancerHomework/load_balancer_topo.py
@@ -18,21 +18,17 @@ def myNetwork():
     h3 = net.addHost('h3', ip='192.168.1.3')
     h4 = net.addHost('h4', ip='192.168.1.4')
     #2 server dietro al VIP 192.168.1.100 (gestito dal controller)
-    srv1 = net.addHost('srv1', ip='192.168.1.101')
-    srv2 = net.addHost('srv2', ip='192.168.1.102')
+    srv1 = net.addHost('srv1', ip='192.168.1.101',  mac="00:00:00:00:02:01", port="5")
+    srv2 = net.addHost('srv2', ip='192.168.1.102',  mac="00:00:00:00:02:02", port="6")
 
-    # Collegamenti host ↔ switch
+    # Collegamenti host-switch
     for h in (h1, h2, h3, h4, srv1, srv2):
         net.addLink(h, s1)
-
-    # Start rete
     net.start()
 
     # Collego lo switch al controller Ryu già avviato esternamente (il controller avviato sulla porta 6633)
     s1.cmd('ovs-vsctl set-controller s1 tcp:127.0.0.1:6633')
 
-
-    info('*** Rete pronta. Usa la CLI per testare.\n')
     CLI(net)
     net.stop()
 
